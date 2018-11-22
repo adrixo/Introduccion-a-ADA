0d00fa3fb120d0877adedda935522efe3e9bd547
adrixo <adrianvalrom@gmail.com>
2018-11-22 15:11:49 +0100

    1o

---
+++ 1.ConceptosBasicos/Ejercicio1/src/main.adb
@@ -0,0 +1,13 @@
+with Text_IO;
+
+--Crear una variable de tipo Natural.
+--Introducir un valor por teclado y mostrarlo por pantalla
+procedure main is
+   number :Natural;
+   package Integer_IO is new Text_IO.Integer_IO(Natural);
+
+begin
+   Integer_IO.Get(number);
+   Integer_IO.Put(number);
+
+end main;
---
+++ 1.ConceptosBasicos/Ejercicio3/src/main.adb
@@ -0,0 +1,12 @@
+with Text_IO;
+
+-- Ejercicio3
+-- Crea un tipo numerado que almacene los días de la semana
+-- Crear un subtipo de esta variable con solo días laborables
+procedure Main is
+   type Semana is(Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo);
+   type DiaLaboral is new Semana range Lunes .. Viernes;
+   dia : DiaLaboral;
+begin
+   null;
+end Main;
