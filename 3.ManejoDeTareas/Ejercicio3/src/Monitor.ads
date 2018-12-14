fd50fc5d607362a9aca034620564ac30b824d271
adrixo <adrianvalrom@gmail.com>
2018-12-14 21:19:40 +0100

    Movidos

--- 3.ManejoDeTareas/Ejercicio3/src/Monitor.ads
+++
@@ -1,9 +0,0 @@
-package Monitor is
-
-   protected type contadorCompartido is
-      function lectura return Integer;
-      procedure incrementar;
-      private contador : Integer := 0;
-   end contadorCompartido;
-
-end Monitor;
